2776d9e784b64cb0cced106fbea49dca4b378f62
cxyang <cxyang@strikinglys-MacBook-Pro-2.local>
2017-04-07 20:40:37 +0800

    add website evaluation

--- score-system/evaluation.js
+++ score-system/evaluation.js
@@ -1,6 +1,6 @@
 // owner: chengxuan
 
-const inputURL = 'http://libo.sxl.cn/'
+const inputURL = 'http://www.jameslovesrose.com/'
 const request = require('request')
 const cheerio = require('cheerio')
 const calculator = require('./score-calculation.js')
@@ -16,9 +16,20 @@ function parseHTML(html) {
   const keywords = $('meta[name="keywords"]').attr('content')
   const description = $('meta[name="description"]').attr('content')
 
+
+  const favicon = $('link[rel="shortcut icon"]').attr('href')
+  const shareIcon = $('link[rel="apple-touch-icon"][sizes="76x76"]').attr('href')
+
+  const baiduVerification = html.indexOf("<meta name=\"baidu-site-verification") > -1 
+  const googleVerfication = html.indexOf("analytics_tracker") > -1 
+
+  console.log(googleVerfication)
+
   let score1 = calculator.checkTitle(title)
   let score2 = calculator.checkKeywords(keywords)
   let score3 = calculator.checkDescription(description)
+  let score4 = calculator.checkFavicon(favicon)
+  
 
-  console.log('score:', 100 - (score1 + score1))
+  console.log('score:', 100 - (score1 + score1 + score3 + score4))
 }
