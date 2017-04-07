097ba9adc6a71bbca62ab27a8ee0c6cb14bd5c57
cxyang <cxyang@strikinglys-MacBook-Pro-2.local>
2017-04-07 17:01:35 +0800

    add parsing code

--- score-system/evaluation.js
+++ score-system/evaluation.js
@@ -1,3 +1,16 @@
 // owner: chengxuan
 
-//
+var inputURL = 'http://libo.sxl.cn/';
+var request = require('request')
+var cheerio = require('cheerio')
+var htmlBody;
+
+request(inputURL, function (error, response, body) {
+  	parseHTML(body);
+});
+
+function parseHTML(html){
+
+	const $ = cheerio.load(html);
+	console.log($('title').text());
+}
