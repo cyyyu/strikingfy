21919ff4ee71be709911308a972254906ad4c593
thomasyimgit <thomasyim94@gmail.com>
2017-04-07 18:14:47 +0800

    index

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 import $ from 'jquery'
 
-$(
-  console.log('hello')
-)
+class Evaluation {
+  constructor() {
+    this.PARSE_URL = ''
+  }
+  showEvaluationResult(data) {
+
+  }
+
+  submitHandler = () => {
+    $('.submit').click((e) => {
+      e.preventDefault()
+      const jqxhr = $.post(PARSE_URL, showEvaluationResult)
+        .fail(err => alert('ERROR:', err))
+    })
+  }
+}
+
+$(document).ready(() => {
+  const evalInstance = new Evaluation()
+  evalInstance.submitHandler()
+})
