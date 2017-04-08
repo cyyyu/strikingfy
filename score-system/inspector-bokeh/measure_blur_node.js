05a4a2f6cfabf4abc935bee386f13ff5ff673c5c
Chuang Yu <cyu9960@gmail.com>
2017-04-08 15:09:50 +0800

    fuck

--- score-system/inspector-bokeh/measure_blur_node.js
+++ score-system/inspector-bokeh/measure_blur_node.js
@@ -2,36 +2,47 @@ var fs = require('fs');
 var Canvas = require('canvas');
 var measureBlur = require('./measure_blur.js')
 var image;
+var http = require('http')
 
 function createImage(error, data) {
-    if (error) {
-        console.error('Unable to read image file!');
-        throw error;
-    }
-    image = new Canvas.Image;
-    image.onload = drawImageOnCanvas;
-    image.src = data;
+  if (error) {
+    console.error('Unable to read image file!');
+    throw error;
+  }
+  image = new Canvas.Image;
+  image.onload = drawImageOnCanvas;
+  image.src = data;
 }
 
 function drawImageOnCanvas() {
-    var canvas = new Canvas(),
-        context;
+  var canvas = new Canvas(),
+    context;
 
-    canvas.width = image.width;
-    canvas.height = image.height;
-    context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0);
+  canvas.width = image.width;
+  canvas.height = image.height;
+  context = canvas.getContext('2d');
+  context.drawImage(image, 0, 0);
 
-    showBlurScore(context.getImageData(0, 0, canvas.width, canvas.height))
+  showBlurScore(context.getImageData(0, 0, canvas.width, canvas.height))
 }
 
 function showBlurScore(imageData) {
-    var stats = measureBlur(imageData);
-    console.log('Blur score:', Number((stats.avg_edge_width_perc).toFixed(2)));
-    console.log(stats);
+  var stats = measureBlur(imageData);
+  console.log('Blur score:', Number((stats.avg_edge_width_perc).toFixed(2)));
+  console.log(stats);
 }
 if (process.argv.length >= 3) {
-    fs.readFile(process.argv[2], createImage);
+  let rawData = [];
+  http.get(process.argv[2], (res) => {
+      res.on('data', (chunk) => {
+        rawData.push(chunk)
+      })
+      res.on('end', () => {
+        let buffer = Buffer.concat(rawData)
+        createImage(null, buffer)
+      })
+    })
+    //   fs.readFile(process.argv[2], createImage);
 } else {
-    console.info('Usage: node measure_blur.js path/to/image/file.jpg');
+  console.info('Usage: node measure_blur.js path/to/image/file.jpg');
 }
